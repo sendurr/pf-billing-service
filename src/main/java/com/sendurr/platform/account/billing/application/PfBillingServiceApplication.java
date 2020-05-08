9a6cc52fa7b54b0f76fb8be244e5149604c00415
Sendurr Selvaraj <Sendurr@Sachins-MacBook-Pro-2.local>
2020-05-08 13:35:47 -0700

    initial

---
+++ src/main/java/com/sendurr/platform/account/billing/application/PfBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sendurr.platform.account.billing.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfBillingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PfBillingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sendurr/platform/account/billing/PfBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendurr.platform.account.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfBillingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
