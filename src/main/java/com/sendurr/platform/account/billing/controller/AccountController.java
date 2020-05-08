06fe4828647b5397a8d32069feb6586dc34fc6d4
Sendurr Selvaraj <Sendurr@Sachins-MacBook-Pro-2.local>
2020-05-08 15:30:31 -0700

    added looger

--- src/main/java/com/sendurr/platform/account/billing/controller/AccountController.java
+++ src/main/java/com/sendurr/platform/account/billing/controller/AccountController.java
@@ -1,14 +1,25 @@
 package com.sendurr.platform.account.billing.controller;
 
-import org.springframework.stereotype.Controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class AccountController {
+    Logger logger = LoggerFactory.getLogger(AccountController.class);
+
     @RequestMapping(value = "/accounts", method = RequestMethod.GET)
-    public String getAllAccounts() {
-        return "here are all your accounts";
+    public ResponseEntity<String> getAllAccounts() {
+        logger.info("processing get accounts call");
+
+        ResponseEntity<String> responseEntity = null;
+        responseEntity = new ResponseEntity<>("here are all your accounts", HttpStatus.OK);
+        return responseEntity;
     }
 }
